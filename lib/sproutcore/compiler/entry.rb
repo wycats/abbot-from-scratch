b62ba099796c97668e397333bc259b78895e1ec0
Yehuda Katz <wycats@Yehuda-Katzs-MacBook-Pro-2.local>
2010-11-10 15:15:45 -0600

    Add docs to Entry and make the regex configurable

--- lib/sproutcore/compiler/entry.rb
+++ lib/sproutcore/compiler/entry.rb
@@ -1,16 +1,24 @@
 module SproutCore
   module Compiler
+    # The entry is a simple abstraction that takes in a name and a body
+    # and can extract dependencies from the body based on a pattern
+    #
+    # It will automatically assume that dependencies without an extension
+    # have the same extension as the entry's file name
     class Entry
       attr_reader :name, :body
 
-      def initialize(name, body)
+      DEP_REGEX = %r{\b(?:sc_)?require\(['"]([^'"]*)['"]\)}
+
+      def initialize(name, body, regex = DEP_REGEX)
         @name, @body = name, body
         @extension = name.include?(".") && name.split(".").last
+        @regex = regex
       end
 
       def dependencies
         @dependencies ||= begin
-          @body.scan(%r{\b(?:sc_)?require\(['"]([^'"]*)['"]\)}).map do |match|
+          @body.scan(@regex).map do |match|
             dep = match.last
 
             if !dep.include?(".") && @extension
