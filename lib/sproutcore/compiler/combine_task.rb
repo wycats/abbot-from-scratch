9e558c12cbf6668c6eced3072ca2976615e11128
tomhuda <tomhuda@strobecorp.com>
2011-05-01 13:32:42 -0700

    Update combine task to support other FileTasks as input

--- lib/sproutcore/compiler/combine_task.rb
+++ lib/sproutcore/compiler/combine_task.rb
@@ -5,7 +5,7 @@ module SproutCore
 
       # Create a series of Preprocessor tasks for the inputs
       def self.with_input(directory, glob, package_root)
-        task = define_task(output_location(directory, package_root))
+        task = define_task(output_location(directory))
 
         task.directory = directory
 
@@ -18,9 +18,20 @@ module SproutCore
         [task]
       end
 
+      def self.with_tasks(tasks, directory)
+        task = define_task(output_location(directory))
+        task.directory = directory
+
+        tasks.each do |prereq|
+          task.prerequisites << prereq
+        end
+
+        [task]
+      end
+
       # This method will get updated over time as more types of inputs
       # result in different outputs
-      def self.output_location(input, package_root)
+      def self.output_location(input)
         relative_location = input.sub(%r{^#{Compiler.intermediate}/}, '')
         File.join(Compiler.output, "#{relative_location}.js")
       end
@@ -29,8 +40,9 @@ module SproutCore
         entries = Orderer.new
 
         @prerequisites.map do |task|
-          task_name = task.name.sub(%r{^#{Regexp.escape(directory)}/}, '')
-          entries.add Entry.new(task_name, File.read(task.name))
+          root = File.dirname(directory)
+          entry_name = task.name.sub(%r{^#{Regexp.escape(root)}/}, '')
+          entries.add Entry.new(entry_name, File.read(task.name))
         end
 
         FileUtils.mkdir_p(File.dirname(@name))
